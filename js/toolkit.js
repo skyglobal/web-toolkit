10e5506ef7cc99dce86bd8d4c4896db06d156153
Sky.com Team <dl-skycom@bskyb.com>
2013-07-29 08:37:45 +0100

    PM|VK: explicitly name all files within toolkit

--- js/toolkit.js
+++ js/toolkit.js
@@ -1,4 +1,13 @@
 if (typeof window.define === "function" && window.define.amd) {
-    define(['modules/tabs',
-        'modules/carousel'])
+//    explicitly call all js files here to ensure all files are available
+    define([
+        'utils/hashmanager',
+        'modules/tabs',
+        'modules/carousel'], function(hashmanager, tabs, carousel){
+        return {
+            hashmanager: hashmanager,
+            tabs: tabs,
+            carousel: carousel
+        };
+    });
 }
