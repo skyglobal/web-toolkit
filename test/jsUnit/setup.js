b186d698f787fd23b538e2ae95d912a64bf82d65
Sky.com Team <dl-skycom@bskyb.com>
2013-08-02 09:51:05 +0100

    AG: fixed carousel tests - added polyfill for Function.prototype.bind for phantomjs

--- test/jsUnit/setup.js
+++ test/jsUnit/setup.js
@@ -23,6 +23,30 @@ define(['chai', 'smoax'], function(chai) {
             })
         );
     }
+    //phantomjs doesnt support .bind, so need to polyfill
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function (oThis) {
+            if (typeof this !== "function") {
+                // closest thing possible to the ECMAScript 5 internal IsCallable function
+                throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function () {},
+                fBound = function () {
+                    return fToBind.apply(this instanceof fNOP && oThis
+                        ? this
+                        : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    }
 
     // PhantomJS had some problem if should is set as a global variable and was timing out
     // window.should = should
