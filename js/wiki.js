1473bc872c5c1b7e0d0f0b054cadc3114faeea12
Sky.com Team <dl-skycom@bskyb.com>
2013-08-02 12:28:58 +0100

    PM: Update Demo page. Move code and geek-speak into the console!

--- js/wiki.js
+++ js/wiki.js
@@ -1,14 +1,71 @@
-wiki = (function() {
-    $('#hero').skycom_carousel({
-        carousel: {
-            autoplay: true,
-            videoAds: false
-        }
-    });
-}());
-
 if (typeof window.define === "function" && window.define.amd) {
     define('wiki', ['toolkit'], function() {
-        return wiki;
-    })
+
+        var vars = {
+            lastLog: null
+        };
+
+        function bindEvents(){
+            $('.example .demo').each(function(){
+                var $this = $(this),
+                    selector = $this.attr('data-selector'),
+                    $examples = $this.find('> ' + selector);
+                $examples.each(function(){
+                    $(this).data('html',this.outerHTML);
+                }).on('mouseenter', showCodePreview)
+            });
+
+            $('#hero').skycom_carousel({
+                carousel: {
+                    autoplay: true,
+                    videoAds: false
+                }
+            });
+
+        }
+
+        function showCodePreview(){
+            if (!console){ return; }
+            var $this = $(this),
+                container = $this.closest('.example'),
+                title = container.parent().find('> h2').text(),
+                subtitle = container.find('> h3').text(),
+                html = $this.data('html'),
+                genericNotes = container.parent().find('> .developer-notes').html(),
+                notes = container.find('.developer-notes').html();
+
+            if (vars.lastLog == html) { return; }
+            vars.lastLog = html;
+
+            console.group(title);
+            if (genericNotes && genericNotes.trim().length) {
+                console.log.apply(console,colourCode(genericNotes.trim()));
+            }
+            if (notes && notes.trim().length){
+                console.groupCollapsed('\'' + subtitle + '\' Notes');
+                    console.log.apply(console,colourCode(notes.trim()));
+                console.groupEnd();
+            }
+                console.groupCollapsed('\'' + this.tagName + '\' html');
+                    console.log(html);
+                console.groupEnd();
+            console.groupEnd();
+        }
+
+        function colourCode(str){
+            var codeCount = str.match(/<code>/gi) === null ? 0 : str.match(/<code>/gi).length;
+            var strWithColourCodes = str.replace(/<code>/gi,'%c').replace(/<\/code>/gi,'%c');
+            var returnArr = [strWithColourCodes];
+            if (strWithColourCodes.indexOf('%c')>-1){
+                for (var x=0; x<codeCount; x++){
+                    returnArr.push('background: #FDF6E3; color: #777;');
+                    returnArr.push('background:white; color:black;');
+                }
+            }
+            return returnArr;
+        }
+
+        bindEvents();
+
+    });
 }
