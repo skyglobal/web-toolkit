dc3cc4086a6a7c573b85bba57e2dffd2d03bbf70
Sky.com Team <dl-skycom@bskyb.com>
2013-08-08 12:31:54 +0100

    PM|VK: carousel - improve images on load + add gradient fade

--- js/toolkit.js
+++ js/toolkit.js
@@ -1,9 +1,24 @@
+if (typeof toolkit==='undefined') toolkit={};
+toolkit.main = (function() {
+
+    function bindEvents(){
+        $(window).load(function(){
+            $(document.body).addClass('window-loaded');
+        });
+    }
+
+    bindEvents();
+
+}());
+
+
 if (typeof window.define === "function" && window.define.amd) {
 //    explicitly call all js files here to ensure all files are available
     define([
         'utils/hashmanager',
         'modules/tabs',
         'modules/carousel'], function(hashmanager, tabs, carousel){
+
 
         return {
             hashmanager: hashmanager,
