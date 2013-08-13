0c138a007779cfe7a4af87ac1c618a74603835c4
Sky.com Team <dl-skycom@bskyb.com>
2013-08-13 12:01:10 +0100

    VK: Add window-loaded class after a maximum of 5 seconds even if window load event is not yet triggered, so that related functionality is not blocked because of random assets taking a long time.

--- js/toolkit.js
+++ js/toolkit.js
@@ -1,9 +1,13 @@
 if (typeof toolkit==='undefined') toolkit={};
 toolkit.main = (function() {
 
-    function bindEvents(){
-        $(window).load(function(){
-            $(document.body).addClass('window-loaded');
+    function bindEvents() {
+        var addWindowLoadClass = function() { $(document.body).addClass('window-loaded');},
+            windowLoadTimeout = setTimeout(addWindowLoadClass, 5000);
+
+        $(window).load(function() {
+            clearTimeout(windowLoadTimeout);
+            addWindowLoadClass();
         });
     }
 
