4dd490f6f00d1d9c6a2f01d3386ef31e32015d39
Peter Mouland <peter.mouland@gmail.com>
2013-08-30 09:19:08 +0100

    PM: carousel - accessibility update. Put actions after carousel header. fix action buttons.

--- js/modules/carousel.js
+++ js/modules/carousel.js
@@ -355,7 +355,8 @@ toolkit.carousel = (function(window, $) {
                     }
                     html += '</a>';
                 }
-                $element.find('.skycom-carousel-container').before('<div class="actions">' + html + '</div>').find('> .actions > *').each(function(index) {
+                $element.find('.skycom-carousel-container').before('<div class="actions">' + html + '</div>');
+                $element.find('> .actions > *').each(function(index) {
                     $(this).attr('data-action', actions[index].id).on('click', function(e) {
                         onclick(actions[index].id);
                         e.preventDefault();
