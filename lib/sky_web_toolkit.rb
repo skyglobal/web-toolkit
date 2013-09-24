fd5f3f96b80c87eefdf41128fa58fb20a69b4395
Sky.com Team <dl-skycom@bskyb.com>
2013-09-24 17:14:19 +0100

    PM: cleanup - reorganise folders to keep grunt folders together + add font icons

--- lib/sky_web_toolkit.rb
+++
@@ -1,4 +0,0 @@
-require 'compass'
-Compass::Frameworks.register('sky_web_toolkit',
-                             :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'sass'),
-                             :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
--- sky_web_toolkit.gemspec
+++
@@ -1,7 +0,0 @@
-Gem::Specification.new do |s|
-	s.name = 'sky_web_toolkit'
-	s.version = '0.0.5'
-	s.date = '2013-06-04'
-	s.summary = 'Sky Web Toolkit'
-	s.authors = 'Sky Web Team'
-end
