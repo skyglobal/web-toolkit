a9f1347599b3c8129ce4c6ce866b2870846e3df6
Peter Mouland <peter.mouland@gmail.com>
2013-11-02 18:10:58 +0000

    try to cut down on committed files + start diff tool

---
+++ grunt/js/utils/diff.js
@@ -0,0 +1,54 @@
+define('utils/diff', ['lib/code-mirror'], function(CodeMirror) {
+
+    function bindEvents(){
+        $('#check').on('click', findFiles);
+    }
+
+    function getFile(){
+        var name = $(this).attr('href').replace('#',''),
+            file = '_includes/' + $(this).attr('data-file') + '.html',
+            oldFile = 'http://web-toolkit.global.sky.com/' + $('#check').val() + '/' + file;
+
+        $.ajax({
+            crossDomain: true,
+            url:file,
+            cache: false})
+        .done(function(html){
+                var textarea = $('<textarea id="' + name + '" class="hidden latest"></textarea>').val(html);
+                $('.sky-form').append(textarea);
+            });
+
+        $.ajax({
+            crossDomain: true,
+            url:file,
+            cache: false})
+        .done(function(html){
+                var textarea = $('<textarea id="old-' + name + '" class=hidden></textarea>').val(html);
+                $('.sky-form').append(textarea);
+            });
+    }
+
+    function findFiles(e){
+        $('a[data-file]').each(getFile);
+        return false;
+    }
+
+    function highlight(){
+
+        var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('latest'),
+            {value: document.getElementById('latest').value,lineWrapping: true,lineNumbers: true});
+        var myCodeMirror2 = CodeMirror.fromTextArea(document.getElementById('file2'),
+            {value: document.getElementById('file2').value,lineWrapping: true,lineNumbers: true});
+        function setCookie(c_name, value, exdays) {
+            var exdate = new Date();
+            exdate.setDate(exdate.getDate() + exdays);
+            var c_value = escape(value) + ((exdays === null) ? "" : "; expires=" + exdate.toUTCString());
+            document.cookie = c_name + "=" + c_value;
+        }
+        setCookie("width", document.width, 1000000);
+
+    }
+
+    bindEvents();
+
+});
