ce4f025214d64fc4afe2bfd6539b89f438c7c92c
rachel <dl-skycom@rachel.com>
2013-11-18 16:10:24 +0000

    PM: in page nav

--- grunt/js/modules/tabs.js
+++ grunt/js/modules/tabs.js
@@ -6,200 +6,120 @@
 **/
 if (typeof toolkit==='undefined') toolkit={};
 toolkit.tabs = (function(hash) {
+//    todo: probably needs to turn into a jquery plugin so you can have multiple instances of tabs on the same page
+//    todo: accessibility check when moving tabs about - perhaps dont have 2 separate lists.
+//    todo: move 'more' link to outside the ul
 
-    var $el = {
-        tabContainer: $('section[data-function=tabs]'),
-        tabs: $('section[data-function=tabs] li[role=tab]'),
-        tabTargets: $('section[data-function=tabs] div[role=tabpanel]'),
-        showMore: $('section[data-function=tabs] ul[role=tablist] .dropdown-tab-select a'),
-        dropdownTab: $('section[data-function=tabs] ul[role=tablist] .dropdown-tab-select ul.dropdown-tab-info')
-    };
-    var vars = {
-        rememberState : $el.tabContainer.attr('data-remember-state')==='true',
-        hasSwitchedOut: false,
-        sortDropdown: [],
-        sortTabs: []
-    };
+    var $tabContainer = $('section[data-function=tabs]'),
+        $tabs = $('section[data-function=tabs] li[role=tab]'),
+        $tabTargets = $('section[data-function=tabs] div[role=tabpanel]'),
+        $showMore = $('section[data-function=tabs] .dropdown-tab-select > a'),
+        $moreTabsContainer = $('section[data-function=tabs] .dropdown-tab-select'),
+        $moreTabsLink = $('section[data-function=tabs] .more-tabs');
+
+    var numberOfTabsToShow;
 
     function bindEvents() {
-        if (vars.rememberState){
-            hash.register(getHashList(), changeTab);
-            $el.showMore.on('click', function(e){
-                e.preventDefault();
-                showMore($(this));
-            });
-            $('body').on('click', function (e) {
-                if (!$(e.target).closest($el.showMore).length) {
-                    $($el.showMore).parent("div").children("ul").removeClass('dropdown-tab-selected');
-                    $($el.showMore).removeClass('dropdown-tab-selected');
-                }
-            }); 
-            $el.tabs.on('click', function (e) {
-                if($(e.target).parents('.dropdown-tab-info').length) {
-                    console.log(e.target);
-                }
-            });
-            $(document).ready(function () {
-                changeTabList();
-                showAndHideDots();
-            });
-            $(window).bind('skycom.resizeend', function () {
-                changeTabList();
-                showAndHideDots();
-            });
-        } else {
-            $el.tabs.on('click', function(e){
-                e.preventDefault(); 
-                changeTab($(this).find('a').attr('href'));
-            });
-            $el.showMore.on('click', function(e){
-                e.preventDefault();
-            });    
-        }
+        hash.register(getHashList(), changeTab);
+        $tabs.on('click', function(e){
+            changeTab($(this).find('a').attr('href'));
+        });
+        $showMore.on('click', toggleShowMore);
+        $('body').on('click', hideMore);
+        $(window).bind('skycom.resizeend',  initTabs);
     }
 
     function getHashList() {
         var arrHash = [];
-        $el.tabs.each(function(){
+        $tabs.each(function(){
             arrHash.push($(this).attr('aria-controls'));    
         });
         return arrHash;
     }
 
+    function saveTabOrder(){
+        $tabs.each(function(i){
+            $(this).attr('data-position', i);
+        });
+    }
+
+    function initTabs(){
+        moveTabsToList();
+        moveTabsToDropdown();
+    }
 
     function changeTab(controlId){
-        $el.tabTargets.add($el.tabs).removeClass("selected");
-        $('#' + controlId + '-tab').add($("#" + controlId))
-            .addClass('selected');
-        $this = $("#" + controlId + "-tab");
-        if($this.parents().is("ul.dropdown-tab-info")) {
-            if (vars.hasSwitchedOut) {
-                $($("ul.tabs").children("li").first()).appendTo('.dropdown-tab-info');
-                $this.insertBefore($('ul.tabs').children("li").first());
-                sortDropdown();
-                vars.hasSwitchedOut = true;
-            }
-            else{
-                console.log("dropdown-tab-info");
-                $($("ul.tabs").children("li").last()).prependTo('.dropdown-tab-info');
-                $this.insertBefore($('ul.tabs').children("li").first());
-                sortDropdown();
-                vars.hasSwitchedOut = true;
-            }
-        } else {
-            if (vars.hasSwitchedOut) {
-                $($("ul.tabs").children("li").first()).appendTo('.dropdown-tab-info');
-                $('.dropdown-tab-info').children("li").first().insertAfter($('ul.tabs').children("li").last());
-                sortDropdown();
-                vars.hasSwitchedOut = false;
-                }
-            }
-        
+        controlId = controlId.replace('#!','');
+        var $thisTab = $("#" + controlId + "-tab"),
+            $thisTabTarget = $("#" + controlId);
+        $tabTargets.add($tabs).removeClass("selected");
+        $thisTab.add($thisTabTarget).addClass('selected');
+        initTabs();
     }
 
-    function showMore(showMoreID){
-        if ($(showMoreID).parent("div").children("ul").hasClass('dropdown-tab-selected')) {
-            $($el.showMore).removeClass('dropdown-tab-selected');
-            $(showMoreID).parent("div").children("ul").removeClass('dropdown-tab-selected');
-        } else {
-            $($el.showMore).addClass('dropdown-tab-selected');
-            $(showMoreID).parent("div").children("ul").addClass('dropdown-tab-selected');
-        }
+    function hideMore(e){
+        if ($(e.target).closest($showMore).length) { return; }
+        toggleShowMore('hide');
+    }
+    function toggleShowMore(type){
+        var action = ($moreTabsLink.hasClass('dropdown-tab-selected') || type==='hide') ? 'remove' : 'add';
+        $showMore.add($moreTabsLink)[action + 'Class']('dropdown-tab-selected');
     }
 
-    function showAndHideDots() {
-        $('.dropdown-tab-info').each(function () {
-            if(!$(this).children('li').length) {
-                $(this).parent('div').hide();
-            }
+    function getNumberOfTabsToShow() {
+        var containerWidth = $tabContainer.outerWidth(true) -
+                $moreTabsContainer.show().outerWidth(true) -
+                $tabs.filter('.selected').outerWidth(true) - 10,
+            totalWidth = 0,
+            numberOfTabs = 0;
+        $tabs.not('.selected').attr('style','float:left').each(function () {
+            totalWidth += ($(this).outerWidth(true));
+            if (totalWidth > containerWidth) { return ; }
+            numberOfTabs++;
         });
-    }
-
-    function getNumberOfTabs() {
-        $containerWidth = $el.tabContainer.width();
-        totalWidth = 0;
-        numberOfTabs = 0;
-        $('.tabs [id$=-tab]').each(function () {
-            totalWidth += ($(this).find('a span').innerWidth() + 30);
-            // if $totalWidth is more than $containerWidth then break out
-            if(totalWidth < $containerWidth) {
-                numberOfTabs++;
-            } else if (totalWidth > $containerWidth) {
-                return numberOfTabs;
-            }
-        });
+        $tabs.add($moreTabsContainer).removeAttr('style');
         return numberOfTabs;
     }
 
-    function getNumberOfTabsClicked(e) {
-
+    function moveTabsToList() {
+        $tabs.each(function (i) {
+            $(this).appendTo($tabContainer.find('.tabs'));
+        });
+        sortTabs($tabContainer.find('.tabs'));
+        numberOfTabsToShow = getNumberOfTabsToShow();
     }
 
-    function moveTabsToDropdown(numberOfTabs) {
-        i = 0;
-        $('.tabs').children('li').each(function () {
-            // this moves any lis which should not be in the main tab
-            if(i >= numberOfTabs) {
-                $(this).appendTo('.dropdown-tab-info');
-                sortDropdown();
-                $('.dropdown-tab-select').show(); 
-            } else {
-                i++;
-            }
+    function moveTabsToDropdown() {
+        $tabs.not('.selected').each(function (i) {
+            if(i < numberOfTabsToShow) { return ; }
+            $(this).appendTo($moreTabsLink);
+        });
+        sortTabs($moreTabsLink);
+        $moreTabsContainer.show();
+    }
+
+    function sortTabs($el) {
+        var list = [];
+        $el.find('li').each(function () {
+            list.push($(this).attr('data-position'));
+        });
+        list.sort();
+        $.each(list, function () {
+            $el.find('li[data-position="'+this+'"]').appendTo($el);
         });
     }
 
-
-    function changeTabList() {
-
-        //get width of li elements up to limit
-        numberOfTabs = getNumberOfTabs();
-        
-        //do another loop to start at the numberOfTabs allowed index, and move any after into the dropdown
-        moveTabsToDropdown(numberOfTabs);
-
-        // remove any li's which can fit
-        if (numberOfTabs >= ($('.tabs').children('li').length + 1)) {
-            $('.dropdown-tab-info li').first().insertBefore('.tabs div.dropdown-tab-select');
-            return false;
-        }
-    }
-
-    function sortDropdown() {
-        vars.sortDropdown = [];
-        $('.dropdown-tab-info li').each(function () {
-             vars.sortDropdown.push($(this).attr('data-value'));
-        });
-        vars.sortDropdown.sort();
-        $.each(vars.sortDropdown, function () {
-            $('.dropdown-tab-info li[data-value="'+this+'"').appendTo('.dropdown-tab-info');
-        });
-    }
-
-    function sortTabs() {
-        vars.sortTabs = [];
-        $('.dropdown-tab-info li').each(function () {
-             vars.sortTabs.push($(this).attr('data-value'));
-        });
-        vars.sortTabs.sort();
-        $.each(vars.sortTabs, function () {
-            $('.dropdown-tab-info li[data-value="'+this+'"').appendTo('.dropdown-tab-info');
-        });
-    }
-
-    function init() {
-        bindEvents();
-    }
+    saveTabOrder();
+    bindEvents();
+    initTabs();
 
     return {
-        init: init,   
         getHashList: getHashList,
         changeTab: changeTab,
         moveTabsToDropdown:moveTabsToDropdown
     };
 
 }(toolkit.hashmanager));
-
 
 if (typeof window.define === "function" && window.define.amd) {
     define('modules/tabs', ['utils/hashmanager'], function(hash) {
