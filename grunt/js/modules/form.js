7f5cbd55b25cf91a8b227e5b202002b41fcf56ce
bskyb-mysky <mysky@bskyb.com>
2013-11-21 14:41:11 +0000

    rd: accessible error messages are now a polyfil

--- grunt/js/modules/form.js
+++ grunt/js/modules/form.js
@@ -6,61 +6,78 @@ toolkit.form = (function ($) {
 
     "use strict";
 
-    $('.form-submit').click(function(event) {
-
-        var hasError = false,
-            errors = [];
+    //TODO: feature detect the required attribute
 
 
-        $('#feedback-list-container').remove();
 
-        $('input.required').each(function (index, input) {
-            var inputId    = $(input).attr('id'),
-                $descriptor = $('label.descriptor[for=' + inputId + ']'),
-                $feedbacks = $('label.feedback[for=' + inputId + ']');
+    if (!('required' in document.createElement('input'))) {
 
-            // exit gracefully if we can't find a required form field
-//            if (!$descriptor.length || !$feedbacks.length) {
-//                return;
-//            }
+        $('.sky-form').submit(function(event) {
 
-            if ($(input).val() === '') {
-                hasError = true;
-                $(input).addClass('form-error');
-                $descriptor.addClass('form-error');
-                $feedbacks.removeClass('hidden');
-                errors.push($feedbacks.first());
+            var hasError = false,
+                errors = [];
 
-            } else {
-                $(input).removeClass('form-error');
-                $descriptor.removeClass('form-error');
-                $feedbacks.addClass('hidden');
+
+            $('#feedback-list-container').remove();
+
+            $('input[required]').each(function (index, input) {
+                var inputId     = $(input).attr('id'),
+                    $descriptor = $('label.descriptor[for=' + inputId + ']'),
+                    $feedbacks  = $('label.feedback[for=' + inputId + ']');
+
+                if ($(input).val() === '') {
+                    hasError = true;
+
+                    $descriptor.addClass('form-error');
+
+                    if ($feedbacks.length > 0) {
+                        $feedbacks.removeClass('hidden');
+                    } else {
+                        //create a feedback if one does not exist
+                        $feedbacks = $('<label class="form-error feedback" for="' + $(input).attr('id') + '">' + $descriptor.text() + ' is required</label>').insertAfter($(input));
+                    }
+
+                    if (!$(input).hasClass('form-error')) {
+                        $(input).addClass('form-error');
+                        $('<span class="form-error skycon-warning"></span>').insertAfter($(input));
+                    }
+
+                    errors.push($feedbacks.first());
+
+                } else {
+                    if ($(input).hasClass('form-error')) {
+                        $(input).removeClass('form-error');
+                        $(input).next('.skycon-warning').remove();
+                    }
+                    $descriptor.removeClass('form-error');
+                    $feedbacks.addClass('hidden');
+                }
+            });
+
+            // create list of error messages at the top of the form if there has been any errors
+            if (hasError) {
+                event.preventDefault();
+
+                var errorHtml = '<div id="feedback-list-container" class="row" aria-live="polite"><p><span class="form-error skycon-warning"></span>Please correct the highlighted fields below:</p>',
+                    label,
+                    i;
+
+                errorHtml += '<ul class="feedback-list">';
+
+                for (i = 0; i < errors.length; i++) {
+                    label     = '<label for="' + errors[i].attr("for") + '" class="form-error">' + errors[i].text() + '</label>';
+                    errorHtml += '<li>' + label + '</li>';
+                }
+
+                errorHtml += '</ul></div>';
+
+                $(event.currentTarget).closest('form').prepend(errorHtml);
+
+                // scroll to the top of the forms
+                window.location.href = window.location.href.split('#')[0] + '#feedback-list-container';
             }
         });
-
-        // turn error message on if there has been an error
-        if (hasError) {
-            event.preventDefault();
-
-            var errorHtml = '<div id="feedback-list-container" class="row"><p><span class="form-error skycon-warning"></span>Please correct the highlighted fields below before submitting again.</p>',
-                label,
-                i;
-
-            errorHtml += '<ul class="feedback-list">';
-
-            for (i = 0; i < errors.length; i++) {
-                label     = '<label for="' + errors[i].attr("for") + '" class="form-error">' + errors[i].text() + '</label>';
-                errorHtml += '<li>' + label + '</li>';
-            }
-
-            errorHtml += '</ul></div>';
-
-            $(event.currentTarget).closest('form').prepend(errorHtml);
-
-            // scroll to the top of the forms
-            window.location.href = window.location.href.split('#')[0] + '#feedback-list-container';
-        }
-    });
+    }
 
 })(jQuery);
 
