ebb7c260a0533c9da932c18da5ac28e5b9e96762
bskyb-mysky <mysky@bskyb.com>
2013-11-26 17:13:22 +0000

    Jacek'n'Stevie - Lightbox, Work-In-Progress

---
+++ grunt/js/modules/lightbox.js
@@ -0,0 +1,52 @@
+/*global jQuery:false */
+if (typeof toolkit==='undefined') toolkit={};
+toolkit.lightbox = (function ($) {
+    "use strict";
+
+    console.info("Fo' shizzle my nizzle, da lightbox is here!");
+
+	function Lightbox($element){
+		this.$container = $element;
+		this.$closeIcon = $element.find('.lightbox-close');
+		this.bindEvents();
+	}
+
+	Lightbox.prototype = {
+		bindEvents: function() {
+			// bind the grey faded bits
+			this.$container.on("click", this.close.bind(this));
+
+			// bind the close icon
+			this.$closeIcon.on("click", this.close.bind(this));
+
+			// bind all lightbox open links
+			var lightboxId = this.$container.attr('id');
+			$('[data-skyLightbox=#' + lightboxId + ']').on("click", this.open.bind(this));
+
+			// prevent clicks on the lightbox from closing it
+			this.$container.find('.lightbox-content').on("click", function(e) {
+				return false;
+			}.bind(this));
+		},
+
+		open: function(e) {
+			e.preventDefault();
+			this.$container.addClass('lightbox-open');
+			$('body').addClass('lightbox-active');
+		},
+
+		close: function(e) {
+			e.preventDefault();
+			this.$container.removeClass('lightbox-open');
+			$('body').removeClass('lightbox-active');
+		}
+	};
+
+	$.fn.skyLightbox = function() {
+		return this.each(function() {
+			var lightbox = new Lightbox($(this));
+		});
+	};
+
+    return Lightbox;
+})(jQuery);
