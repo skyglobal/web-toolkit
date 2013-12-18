9fdc451f1aa18e9958ec27a3d630a99bb9d04bb7
Peter Mouland <peter.mouland@gmail.com>
2013-12-18 02:07:36 +0000

    merge in latest master

--- _site/test/specs/inPageNavSpec.js
+++ _site/test/specs/inPageNavSpec.js
@@ -1,30 +1,28 @@
-function inPageNavSpec(hash, tabs){
+function inPageNavSpec(hash, tabs) {
 
-    require(['utils/hashmanager', 'components/inPageNav'],function(hash, tabs){
+    var describeSpec = 'Test responsive tabs';
 
-        $('.tabs-container').inPageNav();
+    describe(describeSpec, function () {
 
-        describe('Test responsive tabs', function() {
-
-            it('check change tab removes class and adds classes for the correct controlID', function(){
-                expect($('#first-tab').hasClass('selected')).to.equal(true);
-                $('#first-tab').click();
-                expect($('#first-tab').hasClass('selected')).to.equal(true);
-                expect($('#second-tab').hasClass('selected')).to.equal(false);
-                $('#fourth-tab').click();
-                expect($('#first-tab').hasClass('selected')).to.equal(false);
-                expect($('#third-tab').hasClass('selected')).to.equal(false);
-                expect($('#fourth-tab').hasClass('selected')).to.equal(true);
-            });
-
+        it('check change tab removes class and adds classes for the correct controlID', function () {
+            expect($('#first-tab').hasClass('selected')).to.equal(true);
+            $('#first-tab').click();
+            expect($('#first-tab').hasClass('selected')).to.equal(true);
+            expect($('#second-tab').hasClass('selected')).to.equal(false);
+            $('#fourth-tab').click();
+            expect($('#first-tab').hasClass('selected')).to.equal(false);
+            expect($('#third-tab').hasClass('selected')).to.equal(false);
+            expect($('#fourth-tab').hasClass('selected')).to.equal(true);
         });
-        mocha.run();
     });
 
+    return describeSpec;
 }
 
 if (window.define) {
-    define('specs/inPageNavSpec', ['utils/hashmanager', 'components/inPageNav'], function (hash, tabs) {
-        return inPageNavSpec(hash, tabs);
-    })
+    define('specs/inPageNavSpec', ['utils/hashManager', 'components/inPageNav'],
+        function (hash, tabs) {
+            return inPageNavSpec(hash, tabs);
+        }
+    );
 }
