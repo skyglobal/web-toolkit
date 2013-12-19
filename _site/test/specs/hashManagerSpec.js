51c5639e8954454f0e84202827ef0ab33b8caa4c
Peter Mouland <peter.mouland@gmail.com>
2013-12-19 23:33:13 +0000

    start to create 2.0.0alpha. fix test

--- _site/test/specs/hashManagerSpec.js
+++ _site/test/specs/hashManagerSpec.js
@@ -1,54 +1,58 @@
-function hashManagerSpec(hash){
+function hashManagerSpec(hash) {
 
-        describe('Hash Utility', function(){
+    var describeSpec = 'Hash Utility';
 
-            it('The window hash will change when calling changeHash', function() {
-                hash.change('newHash')
-                expect(location.hash).to.equal('#!newHash');
-                hash.change('my-this-is-wonderful')
-                expect(location.hash).to.equal('#!my-this-is-wonderful');
-            });
+    if (!hash){ //needed for running test in demo page
+        hash = toolkit.hashManager;
+    }
 
-            it('A hash can be registered to execute a function (async). once.', function(done) {
-                var count = 0,
-                    callback = function(){
-                        count++;
-                        expect(count).to.equal(1);
-                        done();
-                    };
-                hash.register(['hash'], callback);
+    describe(describeSpec, function () {
 
-                location.hash = 'hash';
-
-                try {
-                    hash.register(['hash'], callback);
-                    expect('Error should have been thrown').to.equal(false);
-                } catch(e){
-                    expect(e.message).to.equal('hashManager: hash (hash) already exists');
-                }
-            });
-
+        it('cleans up any occurence of the # character in the hash', function () {
+            expect(hash.cleanHash('#news')).to.equal('news');
+            expect(hash.cleanHash('#sports')).to.equal('sports');
         });
 
-        describe('The Hash Manager', function() {
-
-            it('cleans up any occurence of the # character in the hash', function() {
-                expect(hash.cleanHash('#news')).to.equal('news');
-                expect(hash.cleanHash('#sports')).to.equal('sports');
-            });
-
-            it('also cleans up any occurence of the ! character in the hash', function() {
-                expect(hash.cleanHash('#!news')).to.equal('news');
-                expect(hash.cleanHash('#!sports')).to.equal('sports');
-            });
+        it('also cleans up any occurence of the ! character in the hash', function () {
+            expect(hash.cleanHash('#!news')).to.equal('news');
+            expect(hash.cleanHash('#!sports')).to.equal('sports');
         });
 
-        mocha.run();
+        it('Will execute an assigned function when the documents hash changes', function (done) {
+            var count = 0,
+                callback = function () {
+                    count++;
+                    expect(count).to.equal(1);
+                    done();
+                };
+            hash.register(['unregistered-hash-link'], callback);
 
+            try {
+                hash.register(['unregistered-hash-link'], callback);
+                expect('Error should have been thrown').to.equal(false);
+            } catch (e) {
+                expect(e.message).to.equal('hashManager: hash (unregistered-hash-link) already exists');
+            }
+
+            location.hash = 'unregistered-hash-link';
+        });
+
+        it('can change the documents location with .changeHash()', function () {
+            hash.change('newHash');
+            expect(location.hash).to.equal('#!newHash');
+            hash.change('my-this-is-wonderful');
+            expect(location.hash).to.equal('#!my-this-is-wonderful');
+        });
+
+    });
+
+    mocha.run();
+
+    return describeSpec;
 }
 
 if (window.define) {
     define('specs/hashManagerSpec', ['utils/hashManager'], function (hash) {
         return hashManagerSpec(hash);
-    })
+    });
 }
