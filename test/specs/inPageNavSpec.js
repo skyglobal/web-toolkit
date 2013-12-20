20baaf1fe1a86dc08776a0404ac173cf88501387
Peter Mouland <peter.mouland@gmail.com>
2013-12-20 01:30:00 +0000

    start to create 2.0.0alpha. comment out test test

--- test/specs/inPageNavSpec.js
+++ test/specs/inPageNavSpec.js
@@ -1,25 +1,23 @@
 function inPageNavSpec(hash, tabs){
 
-    require(['utils/hashManager', 'components/inPageNav'],function(hash, tabs){
 
-        $('.tabs-container').inPageNav();
+    $('.tabs-container').inPageNav();
 
-        describe('Test responsive tabs', function() {
+    describe('Test responsive tabs', function() {
 
-            it('check change tab removes class and adds classes for the correct controlID', function(){
-                expect($('#first-tab').hasClass('selected')).to.equal(true);
-                $('#first-tab').click();
-                expect($('#first-tab').hasClass('selected')).to.equal(true);
-                expect($('#second-tab').hasClass('selected')).to.equal(false);
-                $('#fourth-tab').click();
-                expect($('#first-tab').hasClass('selected')).to.equal(false);
-                expect($('#third-tab').hasClass('selected')).to.equal(false);
-                expect($('#fourth-tab').hasClass('selected')).to.equal(true);
-            });
-
+        it('check change tab removes class and adds classes for the correct controlID', function(){
+            expect($('#first-tab').hasClass('selected')).to.equal(true);
+            $('#first-tab').click();
+            expect($('#first-tab').hasClass('selected')).to.equal(true);
+            expect($('#second-tab').hasClass('selected')).to.equal(false);
+            $('#fourth-tab').click();
+            expect($('#first-tab').hasClass('selected')).to.equal(false);
+            expect($('#third-tab').hasClass('selected')).to.equal(false);
+            expect($('#fourth-tab').hasClass('selected')).to.equal(true);
         });
-        mocha.run();
+
     });
+    mocha.run();
 
 }
 
