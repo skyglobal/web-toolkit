40810e6ff0dc97eaa841c17d8dda7736ace59515
Peter Mouland <peter.mouland@gmail.com>
2014-01-02 10:16:13 +0000

    PM: sort out toggle tests

--- _site/test/specs/toggleSpec.js
+++ _site/test/specs/toggleSpec.js
@@ -30,56 +30,14 @@ function toggleSpec(toggle) {
 
         });
 
-
-        it('can toggle by passing the id of the container and state to toggle to', function () {
-
-            var container = $('#toggle-by-container .edit-container');
-            var toggleLink = $("#toggle-by-container .toggle-link");
-
-            expect(container.hasClass('toggle-hidden')).to.be.false;
+        it("when passing a specific toggle action i.e. hide or show", function () {
+            var container = $('#toggle-by-element .edit-container');
+            var toggleLink = $("#toggle-by-element .toggle-link");
 
             toggle({$container:container, action:'hide'});
-            expect(toggleLink.attr("data-toggle-state")).to.equal("hidden");
-            expect(toggleLink.attr("data-toggle-text")).to.equal("Hide");
-            expect(toggleLink.attr("data-tracking-label")).to.equal("Hide");
             expect(toggleLink.text()).to.equal("Show");
-            expect(container.hasClass('toggle-hidden')).to.be.true;
 
             toggle({$container:container, action:'show'});
-            expect(container.hasClass('toggle-hidden')).to.be.false;
-            expect(toggleLink.attr("data-toggle-state")).to.equal("shown");
-            expect(toggleLink.attr("data-toggle-text")).to.equal("Show");
-            expect(toggleLink.attr("data-tracking-label")).to.equal("Show");
-            expect(toggleLink.text()).to.equal("Hide");
-
-        });
-
-        it("only changes the text inside a span if a span exists", function () {
-            var container = $('#toggle-by-container .edit-container');
-            var toggleLink = $("#toggle-by-container .toggle-link");
-            toggle({$container:container, action:'hide'});
-            expect(toggleLink.text()).to.equal("Show");
-            toggle({$container:container, action:'show'});
-            expect(toggleLink.text()).to.equal("Hide");
-            expect(toggleLink.find('i').length).to.equal(1);
-        });
-
-        it('doesnt do anything if the container is already toggled to the state you are trying to change to', function () {
-
-            var container = $('#toggle-already-done .edit-container');
-            var toggleLink = $("#toggle-already-done .toggle-link");
-
-            expect(container.hasClass('toggle-hidden')).to.be.false;
-            expect(toggleLink.attr("data-toggle-state")).to.equal("shown");
-            expect(toggleLink.attr("data-toggle-text")).to.equal("Show");
-            expect(toggleLink.attr("data-tracking-label")).to.equal("Show");
-            expect(toggleLink.text()).to.equal("Hide");
-
-            toggle({$container:container, action:'show'});
-            expect(container.hasClass('toggle-hidden')).to.be.false;
-            expect(toggleLink.attr("data-toggle-state")).to.equal("shown");
-            expect(toggleLink.attr("data-toggle-text")).to.equal("Show");
-            expect(toggleLink.attr("data-tracking-label")).to.equal("Show");
             expect(toggleLink.text()).to.equal("Hide");
 
         });
