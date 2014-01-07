85b6e6b5c5275d4426087c831fb0b47873fe48c4
joey <dl-skycom@joey.com>
2014-01-07 09:19:20 +0000

    AG|ES: move fatter mobile carousel into a separate demo

---
+++ _includes/components/carousel/biggerMobile.js
@@ -0,0 +1,52 @@
+$('#hero-with-bigger-mobile').skycom_carousel({
+    autoplay: true,
+    videoAds: false
+});
+
+function responsiveImages(container) {
+    var w = window,
+        ps = container.getElementsByTagName("div"),
+        i = 0, il = ps.length,
+        j = 0, jl = ps.length,
+        sources, matches, media, picImg, newSrc;
+    for (; i < il; i++) {
+        if (ps[ i ].getAttribute("data-picture") !== null) {
+
+            sources = ps[ i ].getElementsByTagName("div");
+            matches = [];
+
+            // See if which sources match
+            for (j = 0, jl = sources.length; j < jl; j++) {
+                media = sources[ j ].getAttribute("data-media");
+                // if there's no media specified, OR w.matchMedia is supported
+                if (!media || ( w.matchMedia && w.matchMedia(media).matches )) {
+                    matches.push(sources[ j ]);
+                }
+            }
+
+            // Find any existing img element in the picture element
+            picImg = ps[ i ].getElementsByTagName("img")[ 0 ];
+
+            if (matches.length) {
+                if (!picImg) {
+                    picImg = w.document.createElement("img");
+                    picImg.alt = ps[ i ].getAttribute("data-alt");
+                    ps[ i ].appendChild(picImg);
+                }
+                newSrc = matches.pop().getAttribute("data-src");
+                if (picImg.src !== newSrc) {
+                    picImg.src = newSrc;
+                }
+            }
+            else if (picImg) {
+                ps[ i ].removeChild(picImg);
+            }
+        }
+    }
+}
+
+responsiveImages(document.getElementById('hero-with-bigger-mobile'));
+// finishing off the responsive images work from above with this jQuery event binding
+$(window).bind("skycom.resizeend", function () {
+    responsiveImages(document.getElementById('hero-with-bigger-mobile'));
+});
