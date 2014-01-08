c654cd4406f1188b1aa9004daf20e123cce6cf3b
joey <dl-skycom@joey.com>
2014-01-08 14:29:15 +0000

    AG|ES: performance improvement for responsiveImages (and make sure container exists)

--- _includes/utils/responsiveImages/function.js
+++ _includes/utils/responsiveImages/function.js
@@ -3,45 +3,49 @@ var responsiveImages = (function () {
     var containers = [];
 
     function init(container) {
-        var w = window,
-            ps = container.getElementsByTagName("div"),
-            i = 0, il = ps.length,
-            j = 0, jl = ps.length,
-            sources, matches, media, picImg, newSrc;
+        if (container) {
+            var w = window,
+                ps = container.getElementsByTagName("div"),
+                i = 0, il = ps.length,
+                j = 0, jl = ps.length,
+                sources, matches, media, picImg, newSrc;
 
-        containers.push(container);
+            if (containers.indexOf(container) < 0) {
+                containers.push(container);
+            }
 
-        for (; i < il; i++) {
-            if (ps[ i ].getAttribute("data-picture") !== null) {
+            for (; i < il; i++) {
+                if (ps[ i ].getAttribute("data-picture") !== null) {
 
-                sources = ps[ i ].getElementsByTagName("div");
-                matches = [];
+                    sources = ps[ i ].getElementsByTagName("div");
+                    matches = [];
 
-                // See if which sources match
-                for (j = 0, jl = sources.length; j < jl; j++) {
-                    media = sources[ j ].getAttribute("data-media");
-                    // if there's no media specified, OR w.matchMedia is supported
-                    if (!media || ( w.matchMedia && w.matchMedia(media).matches )) {
-                        matches.push(sources[ j ]);
+                    // See if which sources match
+                    for (j = 0, jl = sources.length; j < jl; j++) {
+                        media = sources[ j ].getAttribute("data-media");
+                        // if there's no media specified, OR w.matchMedia is supported
+                        if (!media || ( w.matchMedia && w.matchMedia(media).matches )) {
+                            matches.push(sources[ j ]);
+                        }
                     }
-                }
 
-                // Find any existing img element in the picture element
-                picImg = ps[ i ].getElementsByTagName("img")[ 0 ];
+                    // Find any existing img element in the picture element
+                    picImg = ps[ i ].getElementsByTagName("img")[ 0 ];
 
-                if (matches.length) {
-                    if (!picImg) {
-                        picImg = w.document.createElement("img");
-                        picImg.alt = ps[ i ].getAttribute("data-alt");
-                        ps[ i ].appendChild(picImg);
+                    if (matches.length) {
+                        if (!picImg) {
+                            picImg = w.document.createElement("img");
+                            picImg.alt = ps[ i ].getAttribute("data-alt");
+                            ps[ i ].appendChild(picImg);
+                        }
+                        newSrc = matches.pop().getAttribute("data-src");
+                        if (picImg.src !== newSrc) {
+                            picImg.src = newSrc;
+                        }
                     }
-                    newSrc = matches.pop().getAttribute("data-src");
-                    if (picImg.src !== newSrc) {
-                        picImg.src = newSrc;
+                    else if (picImg) {
+                        ps[ i ].removeChild(picImg);
                     }
-                }
-                else if (picImg) {
-                    ps[ i ].removeChild(picImg);
                 }
             }
         }
@@ -49,8 +53,8 @@ var responsiveImages = (function () {
 
     function bindOnResize() {
         $(window).on("resizeend", function () {
-            var i, n=containers.length;
-            for (i=0;i< n;i++) {
+            var i, n = containers.length;
+            for (i = 0; i < n; i++) {
                 init(containers[i]);
             }
         });
