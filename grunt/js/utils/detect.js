f165113bf2ff8b10907a65282eb9946d52d4e2fc
CircleCI <circleci@bskyb.com>
2014-01-16 21:26:31 +0000

    refactor fonts js + add document.ready into events

--- grunt/js/utils/detect.js
+++ grunt/js/utils/detect.js
@@ -19,67 +19,6 @@ toolkit.detect = (function () {
         $(window).bind('resize', updateDetectionStates);
     }
 
-    function support3D(){
-        var property = 'transform';
-        var style = html.style;
-        for(var i=0; i<vendorPrefix.length; i++) {
-            style[vendorPrefix[i] + property] = 'translate3D(0,0,0)';
-            if (style[vendorPrefix[i] + property] === 'translate3D(0,0,0)'){
-                state.css.support3D = true;
-                return state.css.support3D;
-            }
-        }
-        state.css.support3D = false;
-        return state.css.support3D;
-    }
-
-    function inHtml(property){
-        var style = html.style;
-        if(typeof style[property] == 'string') {
-            state.css[property] = true;
-            return true;
-        }
-        property = property.charAt(0).toUpperCase() + property.substr(1);
-        for(var i=0; i<vendorPrefix.length; i++) {
-            if(typeof style[vendorPrefix[i] + property] == 'string') {
-                state.css[property] = true;
-                return state.css[property];
-            }
-        }
-        state.css[property] = false;
-        return state.css[property];
-    }
-
-    function css(property){
-        if (state.css[property]) { return state.css[property]; }
-        if (property === 'support3D' ){
-            return support3D(property);
-        } else {
-            return inHtml(property);
-        }
-    }
-
-    function pseudo(el, pos){
-        if (!window.getComputedStyle) { return false; }
-        var css = window.getComputedStyle(el, ':' + pos);
-        var str = (css.getPropertyValue('content') && css.getPropertyValue('content')!='normal') ? css.getPropertyValue('content') : css.getPropertyValue('font-family');
-        return (str)? str.replace(/"/g,'').replace(/'/g,'') : '';
-    }
-
-    function view(type){
-        state.view = pseudo(html,'after') || 'desktop';
-        return (type) ? state.view == type : state.view ;
-    }
-
-    function orientation(type){
-        state.orientation = pseudo(html, 'before') || 'landscape';
-        return (type) ? state.orientation == type : state.orientation;
-    }
-
-    function touch(){
-        state.touch = (!!window.ontouchstart);
-        return state.touch;
-    }
 
     function updateDetectionStates(){
         removeClasses();
@@ -105,6 +44,96 @@ toolkit.detect = (function () {
         html.className = arrClasses.join(' ');
     }
 
+    function support3D(){
+        var property = 'transform';
+        var style = html.style;
+        for(var i=0; i<vendorPrefix.length; i++) {
+            style[vendorPrefix[i] + property] = 'translate3D(0,0,0)';
+            if (style[vendorPrefix[i] + property] === 'translate3D(0,0,0)'){
+                state.css.support3D = true;
+                return state.css.support3D;
+            }
+        }
+        state.css.support3D = false;
+        return state.css.support3D;
+    }
+
+    function supportsPsuedo(){
+        var doc = document,
+            html = doc.documentElement,
+            body = doc.body,
+            supportsPseudo = false,
+            paraBefore = doc.createElement('p'),
+            styleBefore = doc.createElement('style'),
+            heightBefore,
+            selectorsBefore = '#testbefore:before { content: "before"; }';
+
+        styleBefore.type = 'text\/css';
+        paraBefore.id = 'testbefore';
+
+        if (styleBefore.styleSheet) {
+            styleBefore.styleSheet.cssText = selectorsBefore;
+        } else {
+            styleBefore.appendChild(doc.createTextNode(selectorsBefore));
+        }
+
+        body.appendChild(styleBefore);
+        body.appendChild(paraBefore);
+
+        heightBefore = doc.getElementById('testbefore').offsetHeight;
+
+        if (heightBefore >= 1) {
+            supportsPseudo = true;
+        }
+
+        body.removeChild(styleBefore);
+        body.removeChild(paraBefore);
+        return supportsPseudo;
+    }
+
+    function getPseudoClass(el, pos){
+        if (!window.getComputedStyle) { return false; }
+        var css = window.getComputedStyle(el, ':' + pos);
+        var str = (css.getPropertyValue('content') && css.getPropertyValue('content')!='normal') ? css.getPropertyValue('content') : css.getPropertyValue('font-family');
+        return (str)? str.replace(/"/g,'').replace(/'/g,'') : '';
+    }
+
+    function css(property){
+        if (state.css[property]) { return state.css[property]; }
+        if (property === 'support3D' ){
+            return support3D(property);
+        }
+        var style = html.style;
+        if(typeof style[property] == 'string') {
+            state.css[property] = true;
+            return true;
+        }
+        property = property.charAt(0).toUpperCase() + property.substr(1);
+        for(var i=0; i<vendorPrefix.length; i++) {
+            if(typeof style[vendorPrefix[i] + property] == 'string') {
+                state.css[property] = true;
+                return state.css[property];
+            }
+        }
+        state.css[property] = false;
+        return state.css[property];
+    }
+
+    function view(type){
+        state.view = getPseudoClass(html,'after') || 'desktop';
+        return (type) ? state.view == type : state.view ;
+    }
+
+    function orientation(type){
+        state.orientation = getPseudoClass(html, 'before') || 'landscape';
+        return (type) ? state.orientation == type : state.orientation;
+    }
+
+    function touch(){
+        state.touch = (!!window.ontouchstart);
+        return state.touch;
+    }
+
     function elementVisibleBottom($el) {
         if ($el.length < 1) { return; }
 
@@ -119,7 +148,8 @@ toolkit.detect = (function () {
         touch: touch,
         orientation: orientation,
         view: view,
-        pseudo: pseudo,
+        pseudo: getPseudoClass,
+        supportsPsuedo: supportsPsuedo,
         state: state,
         elementVisibleBottom: elementVisibleBottom
     };
