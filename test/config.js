18d6c5554c49bb7faae0aeb40ca4ba2e287fb8b7
rachel <dl-skycom@rachel.com>
2014-02-06 14:54:13 +0000

    PM: test refactor - remove some js duplication

---
+++ test/config.js
@@ -0,0 +1,24 @@
+window.environment = 'test';
+
+window.turnOffAnimation = function(selector){
+    var offTime = '10ms'; //can't be zero as we still need the 'end' events to fire.
+    if (selector){
+        $("<style type='text/css' class='turnOffAnimation'> body " + selector + "{ transition-duration:" + offTime + "!important;-webkit-transition-duration:" + offTime + "!important; -webkit-animation-duration: " + offTime + "!important;animation-duration:" + offTime + "!important;} </style>").appendTo("head");
+    } else {
+        $('.turnOffAnimation').remove();
+    }
+};
+
+window.addScript = function(route, feature, item){
+    var script = document.createElement('script');
+    script.src = '_includes/' + route + '/' + feature + '/' + item + '.js';
+    document.head.appendChild(script);
+};
+
+window.chai = chai;
+window.assert = chai.assert;
+window.expect = chai.expect;
+window.to = chai.to;
+
+mocha.setup('bdd');
+mocha.setup({ignoreLeaks: true});
