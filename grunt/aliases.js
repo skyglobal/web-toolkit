462c0c8ad0cc5db6d599e4d283744c279d07c3de
Peter Mouland <peter.mouland@gmail.com>
2014-02-19 22:58:52 +0000

    introduce `grunt server` and remove the need to run `jekyll serve`

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -5,6 +5,7 @@ module.exports = {
     //  standard build tasks that lints your JS
     "build": ['jshint', 'dev-build'],
     "spy": ['jshint', 'dev-spy'],
+    "serve": ['build','connect:app', 'open:app', 'watch'],
 
     //  misc tasks
     "fonts": ['clean:css', 'clean:fonts', 'svgmin:fonts', 'webfont', 'compass:toolkit'],
@@ -13,10 +14,11 @@ module.exports = {
     //  testing tasks
     "test-with-coverage": ['requirejs:beautify','jekyll:build', 'blanket_mocha'],
     "test-without-coverage": ['requirejs:uglify','jekyll:build', 'mocha'],
-    "test-cross-browser": ['jekyll:build','connect', 'exec:browserstack'],
-    "test-cross-browser-live": ['jekyll:build','connect', 'exec:browserstack-live'],
+    "test-cross-browser": ['jekyll:build','connect:cross-browser', 'exec:browserstack'],
+    "test-cross-browser-live": ['jekyll:build','connect:cross-browser', 'exec:browserstack-live'],
 
     //  alias
+    "server": ['serve'],
     "test": ['test-with-coverage'],
     "default": ['build']
 };
--- grunt/connect.js
+++ grunt/connect.js
@@ -1,8 +1,15 @@
 module.exports = {
-    server: {
-        options: {
-            base: "_site",
-            port: 9999
-        }
-    }
+   "cross-browser": {
+       options: {
+           base: "_site",
+           port: 9999
+       }
+   },
+   app: {
+       options: {
+           port: 4000,
+           base: "_site",
+           hostname: '0.0.0.0'
+       }
+   }
 };
