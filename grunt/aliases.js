7539c47eaa0d6304931b7185c5bb5dec41aa64be
Peter Mouland <peter.mouland@gmail.com>
2014-02-19 14:38:41 +0000

    fix grunt aliases

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -1,24 +1,22 @@
-module.exports = function(grunt){
-    return {
-        "dev-build": ['clean:toolkit', 'compass:toolkit', 'requirejs:toolkit', 'jekyll:build'],
-        "dev-spy": ['dev-build', 'watch'],
+module.exports = {
+    "dev-build": ['clean:toolkit', 'compass:toolkit', 'requirejs:toolkit', 'jekyll:build'],
+    "dev-spy": ['dev-build', 'watch'],
 
-        //  standard build tasks that lints your JS
-        "build": ['jshint', 'dev-build'],
-        "spy": ['jshint', 'dev-spy'],
+    //  standard build tasks that lints your JS
+    "build": ['jshint', 'dev-build'],
+    "spy": ['jshint', 'dev-spy'],
 
-        //  misc tasks
-        "fonts": ['clean:css', 'clean:fonts', 'svgmin:fonts', 'webfont', 'compass:toolkit'],
-        "svgs": ['svgmin:icons', 'grunticon'],
+    //  misc tasks
+    "fonts": ['clean:css', 'clean:fonts', 'svgmin:fonts', 'webfont', 'compass:toolkit'],
+    "svgs": ['svgmin:icons', 'grunticon'],
 
-        //  testing tasks
-        "test-with-coverage": ['requirejs:beautify','jekyll:build', 'blanket_mocha'],
-        "test-without-coverage": ['requirejs:uglify','jekyll:build', 'mocha'],
-        "test-cross-browser": ['jekyll:build','connect', 'exec:browserstack'],
-        "test-cross-browser-live": ['jekyll:build','connect', 'exec:browserstack-live'],
+    //  testing tasks
+    "test-with-coverage": ['requirejs:beautify','jekyll:build', 'blanket_mocha'],
+    "test-without-coverage": ['requirejs:uglify','jekyll:build', 'mocha'],
+    "test-cross-browser": ['jekyll:build','connect', 'exec:browserstack'],
+    "test-cross-browser-live": ['jekyll:build','connect', 'exec:browserstack-live'],
 
-        //  alias
-        "test": ['test-with-coverage'],
-        "default": ['build']
-    }
+    //  alias
+    "test": ['test-with-coverage'],
+    "default": ['build']
 };
