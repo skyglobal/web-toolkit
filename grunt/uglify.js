b111ca87c124fda8c96ae98cc90ec3d49ed09b02
Peter Mouland <peter.mouland@gmail.com>
2014-02-20 00:37:02 +0000

    added separate toolkit.min.js file added release date + version number to both toolkit js files

---
+++ grunt/uglify.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt){
+    var pkg = grunt.file.readJSON('package.json');
+    return {
+        options: {
+            banner: '/*! ' + pkg.name + ' - v' + pkg.version + ' - ' +
+                grunt.template.today("yyyy-mm-dd") + ' */\n'
+        },
+        min: {
+            options:{
+                beautify: true,
+                mangle: false,
+                compress: false
+            },
+            files: {
+                'dist/scripts/toolkit.min.js': ['dist/scripts/toolkit.js']
+            }
+        },
+        beauty: {
+            options:{
+              beautify: true,
+              mangle: false,
+              compress: false
+            },
+            files: {
+                'dist/scripts/toolkit.js': ['dist/scripts/toolkit.js']
+            }
+        }
+    };
+};
