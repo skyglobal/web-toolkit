1cde25b6a4ef3dd1410e56f6905dcf6be2bdb40b
Fletcher, Ben <ben.fletcher@bskyb.com>
2014-02-20 13:03:10 +0000

    BF: changed reporting style to Spec from dot

--- Gruntfile.js
+++ Gruntfile.js
@@ -159,6 +159,7 @@ module.exports = function(grunt) {
         blanket_mocha: {
             all : ['_site/test.html'],
             options : {
+                reporter: 'Spec',
                 threshold : 70, // <- percetage of files that have to pass coverage rules
                 globalThreshold : 80, // <- coverage rule
                 log : true
