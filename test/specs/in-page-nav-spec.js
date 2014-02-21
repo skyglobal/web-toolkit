5d2fd41559a7524113c23737cffb9b6bfdcdb260
ross <dl-skycom@bskyb.com>
2014-02-21 17:24:51 +0000

    AG: whole page nav changes

--- test/specs/in-page-nav-spec.js
+++ test/specs/in-page-nav-spec.js
@@ -24,6 +24,24 @@ function inPageNavSpec(hash, tabs) {
             $('#fourth-tab').click();
             expect($('#demo-inpage-nav-tabs .tab.selected').length).to.equal(1);
         });
+
+        describe('Whole Page Navigation', function() {
+            it('change tab look when changing tabs', function () {
+                $('#whole-page-first-tab').click();
+                expect($('#first-tab').hasClass('selected')).to.equal(true);
+                expect($('#fourth-tab').hasClass('selected')).to.equal(false);
+                $('#whole-page-fourth-tab').click();
+                expect($('#fourth-tab').hasClass('selected')).to.equal(true);
+                expect($('#first-tab').hasClass('selected')).to.equal(false);
+            });
+            it('will only have one tab selected at a time', function () {
+                $('#whole-page-first-tab').click();
+                expect($('#demo-inpage-nav-tabs .tab.selected').length).to.equal(1);
+                $('#whole-page-fourth-tab').click();
+                expect($('#demo-inpage-nav-tabs .tab.selected').length).to.equal(1);
+                expect(window.location.href.indexOf('#!whole-page-fourth-tab')).to.be.true;
+            });
+        });
     });
 
     return describeSpec;
