2bbc4f287a05b26469d326bc6f3023d8f2b54a89
Ben Fletcher <bjfletcher@gmail.com>
2014-02-26 11:59:19 +0000

    BF: merged screenshot comparison and verification tasks to the new modular gruntfile scheme in master

--- grunt/mocha.js
+++ grunt/mocha.js
@@ -1,4 +1,7 @@
 module.exports = {
+    // IMPORTANT NOTE:
+    // grunt-mocha-screenshot is grunt-mocha with screenshotting support
+    // so if grunt-mocha needs to be updated, merge (pull) grunt-mocha-screenshot
     all : ['_site/test-without-coverage.html'],
     options : {
         reporter: 'Spec'
