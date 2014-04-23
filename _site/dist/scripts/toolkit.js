69071620aa44e38c597f76d9286ad0725ed98f01
Peter Mouland <peter.mouland@gmail.com>
2014-04-23 15:59:07 +0100

    resolves #24

--- _site/dist/scripts/toolkit.js
+++ _site/dist/scripts/toolkit.js
@@ -1,4 +1,4 @@
-/*! web-toolkit - v2.2.12 - 2014-04-23 */
+/*! web-toolkit - v2.2.12-feature-spinner-update - 2014-04-23 */
 if (typeof toolkit === "undefined") toolkit = {};
 
 toolkit.polyfill = function() {
