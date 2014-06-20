6ca8d59ec019b2bb51cc635f24c9de9c0767a132
Ben Fletcher <bjfletcher@gmail.com>
2014-06-20 11:04:18 +0100

    better unit tests for responsive images

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,13 @@
 
 ## 2.2.13 (next release)
 
- * Skycons
+  * Toolkit
     * [added] Toolkit can now be loaded before the body tag (e.g., in the head tag) without any JS errors
+  * Responsive Images
+    * [updated] Better unit tests for responsive images
+  * Tooltip
+    * [fixed] Tooltip will now show when at the bottom of the window
+ * Skycons
     * [updated] Updated skycons JS module with the latest icons (for IE support)
     * [updated] Pending icon improved (a clock instead of one that looks like reload)
     * [added] Calendar, edit and home icons
--- grunt/blanket_mocha.js
+++ grunt/blanket_mocha.js
@@ -2,13 +2,12 @@ module.exports = {
     all : ['_site/test.html'],
     options : {
         reporter: 'Spec',
-        threshold : 75, // <- each file has to have this much coverage
-        globalThreshold : 80, // <- average coverage
+        threshold : 76, // <- each file has to have this much coverage
+        globalThreshold : 82, // <- average coverage
         // the below is temporary, there is an issue on GitHub to get them up to 75
         customThreshold: {
             'src/js/toolkit/components/tooltip.js': 41,
             'src/js/toolkit/utils/polyfill.js': 67,
-            'src/js/toolkit/utils/responsive-images.js': 40,
             'src/js/toolkit/utils/skycons.js': 54,
             'src/js/toolkit/utils/validation.js': 44
         },
