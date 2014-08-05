c409ef1cdff06b515ca06622e69027ff21bc9642
Ben Fletcher <bjfletcher@gmail.com>
2014-08-05 12:55:26 +0100

    BF: simplified gh-pages down to a simple hello world page to determine if gh-pages is pushing to github.io

--- _includes/utils/event/ready.js
+++
@@ -1,5 +0,0 @@
-function updateEventReady(){
-    document.getElementById('css-demo-event-ready').innerHTML = 'Woah, document loaded.';
-}
-
-toolkit.event.ready(updateEventReady);
--- test/specs/global-spec.js
+++
@@ -1,8 +0,0 @@
-describe('global (non-AMD) toolkit', function () {
-
-    it('should not throw an error', function () {
-        expect(toolkit).to.not.equal(undefined);
-        expect(toolkit.detect).to.not.equal(undefined);
-    });
-
-});
